32845cdb2d6ea3f2ccbb433c69676e0d60119744
Gaurab Rayamajhi <106250922+Gaurab2000@users.noreply.github.com>
2022-05-29 13:28:11 +0545

    Add files via upload

---
+++ ifelifelse.py
@@ -0,0 +1,10 @@
+print("enter your age")
+age=input()
+age=int(age)
+if(age>18):
+    print("you are allowed to drive")
+elif(age<18):
+    print("you are not allowed to drive")
+else:
+    print("we cannot decide you have to visit physically")
+
