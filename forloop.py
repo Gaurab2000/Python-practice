98a461e2f823b3cabf5a3a894c36c429cb79b5a9
Gaurab Rayamajhi <106250922+Gaurab2000@users.noreply.github.com>
2022-05-31 19:46:54 +0545

    python

---
+++ forloop.py
@@ -0,0 +1,7 @@
+dict1= {"Best Python Course": "gaurabcourseCodeW",
+        "Best C Languge Course": "gaurabcourseCodeW,
+        "Harry Sir":"gaurabcourseCodeW"
+        }
+
+for x,y in dict1.items():
+    print(x, y)
