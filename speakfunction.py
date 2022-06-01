2d7811002fac09f62cfc0189fd22b1cdeee45c6e
Gaurab Rayamajhi <106250922+Gaurab2000@users.noreply.github.com>
2022-06-01 19:21:57 +0545

    python

---
+++ speakfunction.py
@@ -0,0 +1,12 @@
+from threading import main_thread
+import pyttsx3
+
+engine=pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[1].id)
+engine.setProperty('voice',voices[0].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    if __name__=="__main__":
+      speak("gaurab is a good boy")
