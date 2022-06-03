fbeee2b8e1307c936eba0d5cf72082bc08074495
Gaurab Rayamajhi <106250922+Gaurab2000@users.noreply.github.com>
2022-06-03 20:02:01 +0545

    python

---
+++ breakcontinue.py
@@ -0,0 +1,9 @@
+i=0
+while(True):
+    if i+1<5:
+        i = i + 1
+        continue
+    print (i+1, end =" ") #prints every number in one line
+    if (i==44):
+               break #stops the running loop
+    i=i+1
