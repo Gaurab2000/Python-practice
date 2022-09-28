a7d6a3fab68adfa3d3928fb12cd11d1533f4f2c8
Gaurab <106250922+Gaurab2000@users.noreply.github.com>
2022-09-28 17:18:41 +0545

    generating random integer

---
+++ randommodule.py
@@ -0,0 +1,9 @@
+import random
+random_integer=random.randint(1,10)
+print(random_integer)
+random_float=random.random()
+print(random_float)
+random_float*5
+print(random_float)
+love_Score=random.randint(1,100)
+print(f"your love score is {love_Score}")
