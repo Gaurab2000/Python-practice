109674f30e397484c3c5068f13eb7e3560fe1b21
Gaurab <106250922+Gaurab2000@users.noreply.github.com>
2022-10-30 16:57:20 +0545

    100 days of python day 5

---
+++ randombillsplitter.py
@@ -0,0 +1,11 @@
+import random 
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+#Get the total number of items in list 
+num_items= len(names)
+
+#Generate random nummbers between 0 and the last index
+random_choice= random.randint(0,num_items-1)
+person_who_will_pay= names[random_choice]
+print(person_who_will_pay + "is going to buy the meal today !")
